c027c69536eab0108d5dc1f8f8d5af59d2596c3b
Bryan McClellan <mccbryan.ops@gmail.com>
2022-05-10 19:25:07 -0500

    workflow

---
+++ refresh.sh
@@ -0,0 +1,14 @@
+!#/bin/sh
+
+git rebase origin container-pass
+git rebase origin container-fail
+git rebase origin iac-pass
+git rebase origin iac-fail
+git checkout container-pass  
+git push --force
+git checkout container-fail 
+git push --force
+git checkout iac-pass  
+git push --force
+git checkout iac-fail  
+git push --force
